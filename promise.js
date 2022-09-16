8c8ea0bbc53ad6c5eb3d0ecc361a26d1da6327c8
lutfi-afandi <lutfiafandi.ofc@gmail.com>
2022-09-16 14:50:41 +0700

    promis belum selesai

--- promise.js
+++ promise.js
@@ -1,7 +1,25 @@
-const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
+const {
+  promiseTheaterIXX,
+  promiseTheaterVGC
+} = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+
+const promiseOutput = (hasil) => {
+  return new Promise((resolve, reject) => {
+    if (hasil == '' || hasil == undefined) {
+      reject('hasil not found!');
+    } else {
+      const ixx = promiseTheaterIXX;
+      const vgc = promiseTheaterVGC;
+      resolve(ixx, vgc);
+    }
+  });
+
+};
+
+
+
 
 module.exports = {
   promiseOutput,
